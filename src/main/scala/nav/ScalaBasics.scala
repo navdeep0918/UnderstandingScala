dedfceff1b59a0c9b37fcfe4f3683a00663c3647
Navdeep Raj <navdeep.raj09@gmail.com>
2024-06-19 16:52:37 +0530

    Initial commit with Scala SBT project setup

---
+++ src/main/scala/nav/ScalaBasics.scala
@@ -0,0 +1,6 @@
+package nav
+
+object ScalaBasics extends App {
+  def helloScala: String = "Hello, Scala!"
+
+}
---
+++ src/test/scala/nav/ScalaTest.scala
@@ -0,0 +1,9 @@
+package nav
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class ScalaTest extends AnyFunSuite {
+  test("Hello, Scala!") {
+    assert(ScalaBasics.helloScala == "Hello, Scala!")
+  }
+}
